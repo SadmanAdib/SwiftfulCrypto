2022a656ffb555d521b95fd18ec544512602d7d1
Sadman Adib <sadmanadib67@gmail.com>
2022-10-24 09:16:42 +0600

    Chart View done. Also added a Date extension.

--- SwiftfulCrypto/Core/Detail/Views/DetailView.swift
+++ SwiftfulCrypto/Core/Detail/Views/DetailView.swift
@@ -40,21 +40,23 @@ struct DetailView: View {
     
     var body: some View {
         ScrollView {
-            VStack(spacing: 20) {
-                Text("")
-                    .frame(height: 150)
+            VStack {
+                ChartView(coin: vm.coin)
+                    .padding(.vertical)
                 
-                overviewTitle
-                
-                Divider()
-                
-                overviewGrid
-                
-                additionalTitle
-                
-                Divider()
-                
-                additionalGrid
+                VStack(spacing: 20) {
+                    overviewTitle
+                    
+                    Divider()
+                    
+                    overviewGrid
+                    
+                    additionalTitle
+                    
+                    Divider()
+                    
+                    additionalGrid
+                }
             }
         }
         .padding()
